96a2195543ae91d064f3b1ac0a2b5e51df8c19f6
JeniaD <jeniadeshchenko@gmail.com>
2024-02-05 23:36:00 +0100

    0.5

--- core.py
+++ core.py
@@ -59,7 +59,7 @@ class Target:
 
 class Core:
     def __init__(self, target="127.0.0.1") -> None:
-        self.version = "0.4"
+        self.version = "0.5"
         self.userAgent = "webzir/" + self.version
 
         self.target = Target()
